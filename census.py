5b47af185d10f76981301d3bc60a7bfef7161c9d
Ned Batchelder <ned@nedbatchelder.com>
2018-03-14 16:38:38 -0400

    Just indentation

--- census.py
+++ census.py
@@ -243,106 +243,106 @@ def text_report(in_file):
             print(f"    {strategy}: {line}")
 
 def html_report(out_file, sites, old, new, all_courses=None, all_orgs=None):
-        CSS = """\
-            html {
-                font-family: sans-serif;
-            }
+    CSS = """\
+        html {
+            font-family: sans-serif;
+        }
 
-            pre {
-                font-family: Consolas, monospace;
-            }
+        pre {
+            font-family: Consolas, monospace;
+        }
 
-            .url {
-                font-weight: bold;
-            }
-            .strategy {
-                font-style: italic;
-            }
-            .tag {
-                display: inline-block;
-                background: #cccccc;
-                font-size: 75%;
-                margin: 0 .1em .1em;
-                padding: 0 .3em;
-                border-radius: 3px;
-            }
-            .tag.slow {
-                background: #d5efa7;
-            }
-            .tag.none {
-                background: #f09393;
-            }
-            .tag.drastic {
-                background: #ebe377;
-            }
+        .url {
+            font-weight: bold;
+        }
+        .strategy {
+            font-style: italic;
+        }
+        .tag {
+            display: inline-block;
+            background: #cccccc;
+            font-size: 75%;
+            margin: 0 .1em .1em;
+            padding: 0 .3em;
+            border-radius: 3px;
+        }
+        .tag.slow {
+            background: #d5efa7;
+        }
+        .tag.none {
+            background: #f09393;
+        }
+        .tag.drastic {
+            background: #ebe377;
+        }
 
-        """
+    """
 
-        writer = HtmlOutlineWriter(out_file, css=CSS, title=f"Census: {len(sites)} sites")
-        header = f"{len(sites)} sites: {old}"
-        if new != old:
-            header += f" &rarr; {new}"
-        writer.start_section(header)
-        for site in sites:
-            old, new = site.latest_courses, site.current_courses
-            tags = []
+    writer = HtmlOutlineWriter(out_file, css=CSS, title=f"Census: {len(sites)} sites")
+    header = f"{len(sites)} sites: {old}"
+    if new != old:
+        header += f" &rarr; {new}"
+    writer.start_section(header)
+    for site in sites:
+        old, new = site.latest_courses, site.current_courses
+        tags = []
 
-            def tag_it(text, tag_name=None):
-                tags.append(f"<span class='tag {tag_name or text.lower()}'>{text}</span>")
-            new_text = ""
-            if new is None:
-                tag_it("None")
-            else:
-                if new != old:
-                    new_text = f"<b> &rarr; {new}</b>"
-                if abs(new - old) > 10 and not (0.5 >= old/new >= 1.5):
-                    tag_it("Drastic")
-            if site.is_gone_now:
-                tag_it("Gone")
-            elif site.is_gone:
-                tag_it("Back")
-            # Times are not right now that we limit requests, not sites.
-            #if site.time > 5:
-            #    tag_it(f"{site.time:.1f}s", "slow")
-            writer.start_section(f"<a class='url' href='{site.url}'>{site.url}</a>: {old}{new_text} {''.join(tags)}")
-            for strategy, tb in site.tried:
-                if tb is not None:
-                    lines = tb.splitlines()
-                    if len(lines) > 1:
-                        line = tb.splitlines()[-1][:100]
-                        writer.start_section(f"<span class='strategy'>{strategy}:</span> {escape(line)}")
-                        writer.write("""<pre class="stdout">""")
-                        writer.write(escape(tb))
-                        writer.write("""</pre>""")
-                        writer.end_section()
-                    else:
-                        writer.write(f"<p>{strategy}: {lines[0]}")
+        def tag_it(text, tag_name=None):
+            tags.append(f"<span class='tag {tag_name or text.lower()}'>{text}</span>")
+        new_text = ""
+        if new is None:
+            tag_it("None")
+        else:
+            if new != old:
+                new_text = f"<b> &rarr; {new}</b>"
+            if abs(new - old) > 10 and not (0.5 >= old/new >= 1.5):
+                tag_it("Drastic")
+        if site.is_gone_now:
+            tag_it("Gone")
+        elif site.is_gone:
+            tag_it("Back")
+        # Times are not right now that we limit requests, not sites.
+        #if site.time > 5:
+        #    tag_it(f"{site.time:.1f}s", "slow")
+        writer.start_section(f"<a class='url' href='{site.url}'>{site.url}</a>: {old}{new_text} {''.join(tags)}")
+        for strategy, tb in site.tried:
+            if tb is not None:
+                lines = tb.splitlines()
+                if len(lines) > 1:
+                    line = tb.splitlines()[-1][:100]
+                    writer.start_section(f"<span class='strategy'>{strategy}:</span> {escape(line)}")
+                    writer.write("""<pre class="stdout">""")
+                    writer.write(escape(tb))
+                    writer.write("""</pre>""")
+                    writer.end_section()
                 else:
-                    writer.write(f"<p>{strategy}: worked</p>")
+                    writer.write(f"<p>{strategy}: {lines[0]}")
+            else:
+                writer.write(f"<p>{strategy}: worked</p>")
+        writer.end_section()
+    writer.end_section()
+
+    if all_courses:
+        total_course_ids = sum(len(sites) for sites in all_courses.values())
+        writer.start_section(f"<p>Course IDs: {total_course_ids}</p>")
+        all_courses_items = sorted(all_courses.items())
+        all_courses_items = sorted(all_courses_items, key=lambda item: len(item[1]), reverse=True)
+        for course_id, sites in all_courses_items:
+            writer.start_section(f"{course_id}: {len(sites)}")
+            for site in sites:
+                writer.write(f"<p><a class='url' href='{site.url}'>{site.url}</a></p>")
             writer.end_section()
         writer.end_section()
 
-        if all_courses:
-            total_course_ids = sum(len(sites) for sites in all_courses.values())
-            writer.start_section(f"<p>Course IDs: {total_course_ids}</p>")
-            all_courses_items = sorted(all_courses.items())
-            all_courses_items = sorted(all_courses_items, key=lambda item: len(item[1]), reverse=True)
-            for course_id, sites in all_courses_items:
-                writer.start_section(f"{course_id}: {len(sites)}")
-                for site in sites:
-                    writer.write(f"<p><a class='url' href='{site.url}'>{site.url}</a></p>")
-                writer.end_section()
+    if all_orgs:
+        shared_orgs = [(org, sites) for org, sites in all_orgs.items() if len(sites) > 1]
+        writer.start_section(f"<p>Shared orgs: {len(shared_orgs)}</p>")
+        for org, sites in sorted(shared_orgs):
+            writer.start_section(f"{org}: {len(sites)}")
+            for site in sites:
+                writer.write(f"<p><a class='url' href='{site.url}'>{site.url}</a></p>")
             writer.end_section()
-
-        if all_orgs:
-            shared_orgs = [(org, sites) for org, sites in all_orgs.items() if len(sites) > 1]
-            writer.start_section(f"<p>Shared orgs: {len(shared_orgs)}</p>")
-            for org, sites in sorted(shared_orgs):
-                writer.start_section(f"{org}: {len(sites)}")
-                for site in sites:
-                    writer.write(f"<p><a class='url' href='{site.url}'>{site.url}</a></p>")
-                writer.end_section()
-            writer.end_section()
+        writer.end_section()
 
 def json_update(sites, all_courses, include_overcount=False):
     data = {}
