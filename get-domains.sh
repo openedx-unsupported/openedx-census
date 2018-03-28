98b201f61d1d0b7ebfa110d8fd0a139ab9b00806
Ned Batchelder <ned@nedbatchelder.com>
2018-03-28 15:21:18 -0400

    Codify how we get referers and aliases

---
+++ get-domains.sh
@@ -0,0 +1,6 @@
+# Get the domains of referers.
+heroku pg:psql -a openedxstats -c "select distinct(domain) from sites_accesslogaggregate" | sed -e 's/^ //' -e 's/\.$//' | grep -E -v -f junk-referers.regex | sort | uniq > referers.txt
+
+# Get the domains of aliases.
+heroku pg:psql -a openedxstats -c "select aliases from sites_site where active_end_date is null and array_length(aliases, 1) > 0;" | sed -E -e '/{/!d' -e 's/[{} ]//g' -e 's/,/\
+/g' | sed -E -e 's/https?:\/\///' -e 's/\/$//' | sort | uniq > aliases.txt
--- get-referers.sh
+++
@@ -1,1 +0,0 @@
-heroku pg:psql -a openedxstats -c "select distinct(domain) from sites_accesslogaggregate" | sed -e 's/^ //' -e 's/\.$//' | grep -E -v -f junk-referers.regex | sort | uniq > referer2.txt
