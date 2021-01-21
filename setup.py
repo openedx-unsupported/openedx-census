48f29f468cfe6f26cf0519a0906b0daadac0ec56
Ned Batchelder <ned@edx.org>
2021-01-21 13:03:00 -0500

    Fix two small recent mistakes
    - tox.ini should be indented with spaces, not tabs.
    
    - The version number shouldn't have been increased to 1.0.0.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def is_requirement(line):
 
 setup(
     name='openedx-census',
-    version='1.0.0',
+    version='0.2.1',
     license='Apache 2',
     description='Open edX census tool',
     author='Ned Batchelder',
