c6a75d00078d946f2fadd544e06a9a1738cf88e9
Ned Batchelder <ned@edx.org>
2021-06-10 10:52:59 -0400

    feat: keep raw referrers separately so we can count them

--- get-domains.sh
+++ get-domains.sh
@@ -1,5 +1,6 @@
 # Get the domains of referers.
-heroku pg:psql -a openedxstats -c "select distinct(domain) from sites_accesslogaggregate" | sed -e 's/^ //' -e 's/\.$//' | grep -E -v -f junk-referers.regex | sort | uniq > refs/referers.txt
+heroku pg:psql -a openedxstats -c "select distinct(domain) from sites_accesslogaggregate" | sed -e 's/^ //' -e 's/\.$//' > refs/raw-referers.txt
+grep -E -v -f junk-referers.regex < refs/raw-referers.txt | sort | uniq > refs/referers.txt
 
 # Get the domains of aliases.
 heroku pg:psql -a openedxstats -c "select aliases from sites_site where active_end_date is null and array_length(aliases, 1) > 0;" | sed -E -e '/{/!d' -e 's/[{} ]//g' -e 's/,/\
